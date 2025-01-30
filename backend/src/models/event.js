4d2e88f177829d7ffba016f78797f32beece5095
replakcan <alper.makcan@gmail.com>
2025-01-30 14:58:20 +0300

    Uses unique id on event models and creates routes

--- backend/src/models/event.js
+++ backend/src/models/event.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose')
 const Question = require('./question')
+const nanoId = require('nanoid')
 
 const Event = new mongoose.Schema({
     title: String,
     questions: [Question],
+    code: {
+        type: String,
+        unique: true
+    },
     participants: [{
         type: 'ObjectId',
         ref: 'User'
     }]
 })
+
+Event.pre("save", async function(next) {
+    if (this.code) return next()
+
+    let code
+
+    do {
+        code = nanoId.nanoid()
+    } while (await mongoose.models["Event"].findOne({ code }))
+    
+    this.code = code
+})
+
 
 module.exports = mongoose.model('Event', Event)
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/event');
 
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/event', async function (req, res, next) {
+  const { title } = req.body
+  const event = new Event({title: title});
+  await event.save();
+
+  console.log(event);
+
+  res.send(event);
+});
+
+router.post('/event/:eventid/questions', async function (req, res, next) {
+  const { eventid } = req.params
+  const { text } = req.body
+  const event = await Event.findById({ eventid })
+  
+  if(!event) return next(new Error('Event not found'))
+
+  event.questions.push({ text })
+
+  try {
+    await event.save()
+    res.send(event)
+  } catch(e) {
+      next(e)
+  }
+
+})
 
 module.exports = router;
