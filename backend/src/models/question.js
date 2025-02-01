52e7c029b1b314ad8be2ac9e477bf4b86a3e85ad
replakcan <alper.makcan@gmail.com>
2025-02-01 16:05:32 +0300

    Adds default values to Question properties
    0 for votes, Anonymous for user

--- backend/src/models/question.js
+++ backend/src/models/question.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 
 const Question = new mongoose.Schema({
     text: String,
-    votes: Number,
+    votes: {
+        type: Number,
+        default: 0
+    },
     user: {
         /* type: 'ObjectId',
         ref: 'User' */
-        type: String
+        type: String,
+        default: "Anonymous"
     },
     is_highlighted: Boolean
 })
